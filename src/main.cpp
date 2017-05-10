acbb032a532090261ced937de90a3c8272d67569
AndyThio <andypthio@gmail.com>
2017-05-10 16:50:31 -0700

    Added average of each call for locks

--- src/main.cpp
+++ src/main.cpp
@@ -206,30 +206,33 @@ void printBt(vector<int> bt){
     rfil.close();
 }
 
-bool isrepeat(const hist_cont &h, gameState c, chrono::duration<double> &astime){
+bool isrepeat(const hist_cont &h, gameState c, chrono::duration<double> &astime_r, unsigned int &asrc){
     chrono::time_point<chrono::system_clock> asstart, asend;
+    ++asrc;
     asstart = chrono::system_clock::now();
     pthread_rwlock_rdlock(&asLock);
     asend = chrono::system_clock::now();
     bool ret = h.find(histNode(c)) != h.end();
     pthread_rwlock_unlock(&asLock);
-    astime += asend - asstart;
+    astime_r += asend - asstart;
     return ret;
 }
 
 void expandDir(gameState g, int parent_depth, hist_cont &alreadyseen, pqType &pq,
-        chrono::duration<double> &astime, chrono::duration<double> &pqtime){
+        chrono::duration<double> &astime_w,chrono::duration<double> &astime_r,
+        chrono::duration<double> &pqtime, unsigned int &asrc, unsigned int &aswc, unsigned int &pqc){
     chrono::time_point<chrono::system_clock> pqstart, pqend;
     chrono::time_point<chrono::system_clock> asstart, asend;
-    
-        if(!isrepeat(alreadyseen, g, astime)){
+
+        if(!isrepeat(alreadyseen, g, astime_r, asrc)){
+            ++aswc;
             asstart = chrono::system_clock::now();
             pthread_rwlock_wrlock(&asLock);
             asend = chrono::system_clock::now();
             alreadyseen.insert(histNode(g));
             pthread_rwlock_unlock(&asLock);
-            astime += asend - asstart;
-    
+            astime_w += asend - asstart;
+
             if(g.isSolved()){
                 isfinished = true;
                 btMut.lock();
@@ -238,13 +241,14 @@ void expandDir(gameState g, int parent_depth, hist_cont &alreadyseen, pqType &pq
                 return;
             }
             int heur = parent_depth+1+g.getheur();
+            ++pqc;
             pqstart = chrono::system_clock::now();
             pqMut.lock();
             pqend = chrono::system_clock::now();
             pq.push(queueNode(g,parent_depth+1,heur));
             pqMut.unlock();
             pqtime += pqend - pqstart;
-            
+
             cpq.notify_one();
         }
 }
@@ -253,13 +257,15 @@ void expandNode(hist_cont &alreadyseen, pqType &pq){
     chrono::time_point<chrono::system_clock> start, end;
     chrono::time_point<chrono::system_clock> pqstart, pqend;
     chrono::time_point<chrono::system_clock> asstart, asend;
-    chrono::duration<double> elapsed_time, astime , pqtime ;
+    chrono::duration<double> elapsed_time, astime_w, astime_r , pqtime ;
+    unsigned int asrc = 0,aswc = 0, pqc = 0;
     bool pqempty;
     queueNode temp;
     chrono::milliseconds ms{100};
+    start = chrono::system_clock::now();
     while(!isfinished){
-        start = chrono::system_clock::now();
-        
+
+        ++pqc;
         pqstart = chrono::system_clock::now();
         pqMut.lock();
         pqend = chrono::system_clock::now();
@@ -270,15 +276,15 @@ void expandNode(hist_cont &alreadyseen, pqType &pq){
         }
         pqMut.unlock();
         pqtime += pqend - pqstart;
-        
+
         while(pqempty){
             unique_lock<mutex> lk(cpqMut);
             cpq.wait_for(lk,ms);
-            
+
             if(isfinished){
                 break;
             }
-            
+            ++pqc;
             pqstart = chrono::system_clock::now();
             pqMut.lock();
             pqend = chrono::system_clock::now();
@@ -293,29 +299,32 @@ void expandNode(hist_cont &alreadyseen, pqType &pq){
         if(isfinished){
             break;
         }
-        
+
         gameState tempr = temp.arena;
         gameState templ = tempr;
         gameState tempu = tempr;
         gameState tempd = tempr;
-    
+
         if(tempr.right()){
-            expandDir(tempr,temp.dept, alreadyseen,pq,astime, pqtime);
+            expandDir(tempr,temp.dept, alreadyseen,pq,astime_w, astime_r, pqtime, asrc, aswc, pqc);
         }
         if(templ.left()){
-            expandDir(templ,temp.dept, alreadyseen,pq, astime, pqtime);
+            expandDir(templ,temp.dept, alreadyseen,pq, astime_w, astime_r, pqtime, asrc, aswc, pqc);
         }
         if(tempd.down()){
-            expandDir(tempd,temp.dept, alreadyseen,pq,astime, pqtime);
+            expandDir(tempd,temp.dept, alreadyseen,pq,astime_w, astime_r, pqtime, asrc, aswc, pqc);
         }
         if(tempu.up()){
-            expandDir(tempu,temp.dept, alreadyseen,pq, astime, pqtime);
+            expandDir(tempu,temp.dept, alreadyseen,pq, astime_w, astime_r, pqtime, asrc, aswc, pqc);
         }
     }
     end = chrono::system_clock::now();
     elapsed_time = end - start;
     printMut.lock();
-    cout << elapsed_time.count() << ", " << pqtime.count() << ", " << astime.count() << ", ";
+    //thread time, pqtime, avgper pq, as write, avg as write, as read, avg asread
+    cout << elapsed_time.count() << ", " << pqtime.count() << ", " << pqtime.count()/pqc << ", "
+        << astime_w.count() << ", " << astime_w.count()/aswc << ", " << astime_r.count() << ", "
+        << astime_r.count()/asrc << ", ";
     printMut.unlock();
 }
 
@@ -325,21 +334,21 @@ void findSolution(gameState s){
     vector<thread> thd;
     gameState finishedState;
     hist_cont alreadyseen;
-    
+
     queueNode temp = pq.top();
-    
+
     while(!isfinished){
         while(thd.size() < max_threads){
             thd.push_back(thread(expandNode,ref(alreadyseen),ref(pq)));
         }
     }
-    
+
     cpq.notify_all();
-    
+
     for(auto &e: thd){
         e.join();
     }
-    
+
     btMut.lock();
     printBt(finalbt);
     btMut.unlock();
@@ -357,15 +366,15 @@ int main(int argc, char* argv[]){
     if(argc > 2){
         max_threads = atoi(argv[2]);
     }
-    
-    
+
+
     pthread_rwlock_init(&asLock, NULL);
     chrono::time_point<chrono::system_clock> start, end;
     chrono::duration<double> elapsed_time;
-    
+
 
     start= chrono::system_clock::now();
-    
+
     findSolution(arena);
 
     end = chrono::system_clock::now();
